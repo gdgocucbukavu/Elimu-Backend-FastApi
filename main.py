3d730856531a76daa61cfc82a754ac4c7be0466e
dorcaswaka <dorcawaka8@gmail.com>
2025-03-20 11:03:06 +0200

    Fusion du dépôt distant et local

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import video, progress
 
 app = FastAPI(title="Elimu Backend")
 
+# Configuration des CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Autoriser toutes les origines (à restreindre en prod)
+    allow_credentials=True,
+    allow_methods=["*"],  # Autoriser toutes les méthodes HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Autoriser tous les headers
+)
+
+# Inclusion des routes
 app.include_router(video.router, prefix="/videos", tags=["Videos"])
 app.include_router(progress.router, prefix="/progress", tags=["Progression"])
 
