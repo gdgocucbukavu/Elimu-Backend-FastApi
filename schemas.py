daee0133592e8732710251f52db18699c09a4e21
Abraham Imani <abrahamfaith325@gmail.com>
2025-03-21 00:50:41 +0200

    Update schemas.py
    Some insight and corrections that can be made.

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional #, List
 
 from pydantic import BaseModel
 from datetime import datetime
@@ -10,11 +10,13 @@ class VideoCreate(BaseModel):
 
 class VideoResponse(VideoCreate):
     id: int
-    title: str
-    description: str
-    publication_date: datetime
+    title: str #Optional[str] = None
+    description: str #Optional[str] = None
+    publication_date: datetime #Optional[str] = None
     views: int
     likes: int
+    # une liset de progressions liées à la vidéo
+    # progresses : List["ProgressResponse"] = []
 
     class Config:
         orm_mode = True
@@ -30,3 +32,15 @@ class VideoUpdate(BaseModel):
 class ProgressCreate(BaseModel):
     video_id: int
     mentee_email: str
+# le schéma de réponse pour une progression 
+"""
+class ProgressResponse(BaseModel):
+    id : int
+    video_id : int
+    mentee_email : str
+    watched : int
+
+    class Config : 
+        orm_mode = True
+
+        """
